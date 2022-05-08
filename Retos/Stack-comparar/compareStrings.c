ddc71067837d222f01cafdfbd21e4a9a046c0dff
Leonel Zamarron <zamarronleo4@gmail.com>
2022-05-07 23:29:25 -0500

    7 de mayo

--- Retos/Stack-comparar/compareStrings.c
+++ Retos/Stack-comparar/compareStrings.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "stack.h"
 
 void dataCapture(String *v);
@@ -14,9 +15,9 @@ int main()
     setup(&string);
 
     dataCapture(&value);
-    push(&string, value);
+    out = push(&string, value);
     dataCapture(&value);
-    push(&string, value);
+    out = push(&string, value);
 
     backSpace(&string, 0);
     backSpace(&string, 1);
@@ -24,39 +25,52 @@ int main()
     out = compareStrings(string);
 
     printf("%d", out);
-    scanf("%d", out);
 
     return 0;
 }
 
 int compareStrings(Stack string)
 {
-    int i, r = 1;
-    while (string.data[0].string[i] != string.data[1].string[i])
+    int i = 0;
+    int r = 0;
+    while ((string.data[0].string[i] == string.data[1].string[i]) && (i < strlen(string.data[0].string)))
     {
-        r = 0;
+        r = 1;
+        i++;
     }
+    if(string.data[0].string[i] != string.data[1].string[i])
+        r = 0;
+
     return r;
 }
 
 void backSpace(Stack *s, int j)
 {
-    for (int i = 0; i < N; i++)
+    int limit = strlen(s->data[j].string);
+
+    for (int i = 0; i < limit; i++)
     {
-        if (s->data[j].string[i] == "#")
+        if (s->data[j].string[0] == *"#")
+        {
+            for (int k = 0; k < limit; k++)
+            {
+                s->data[j].string[k] = s->data[j].string[k+1];
+            }
+        }
+        else if (s->data[j].string[i] == *"#")
         {
             s->data[j].string[i-1] = s->data[j].string[i+1];
 
-            for (int k = i; k < N-2; k++)
+            for (int k = i; k < limit-1; k++)
             {
                 s->data[j].string[k] = s->data[j].string[k+2];
             }
-        }   
+        }
     }
 }
 
 void dataCapture(String *v)
 {
     printf("Introduzca los datos: ");
-    scanf("%s",v->string);
+    scanf("%s", &v->string);
 }
